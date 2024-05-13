6ab86a2d2a2e6ca65eea589f0fec670f3538adcd
AkatsukiRika <m1345860061@gmail.com>
2024-05-13 14:35:22 +0800

    Build Flutter navigation bar

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -30,6 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,11 +39,105 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _chipsRow(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _chipsRow() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+
+      child: Row(
+        children: [
+          _chipsRowItem(text: 'Component Creation', isSelect: selectIndex == 0, onPressed: () {
+            setState(() {
+              selectIndex = 0;
+            });
+          }),
+
+          _chipsRowItem(text: 'Conditional Render', isSelect: selectIndex == 1, onPressed: () {
+            setState(() {
+              selectIndex = 1;
+            });
+          }),
+
+          _chipsRowItem(text: 'Parameter Drilling', isSelect: selectIndex == 2, onPressed: () {
+            setState(() {
+              selectIndex = 2;
+            });
+          }),
+
+          _chipsRowItem(text: 'Event Handling', isSelect: selectIndex == 3, onPressed: () {
+            setState(() {
+              selectIndex = 3;
+            });
+          }),
+
+          _chipsRowItem(text: 'User Input Handling', isSelect: selectIndex == 4, onPressed: () {
+            setState(() {
+              selectIndex = 4;
+            });
+          }),
+
+          _chipsRowItem(text: 'Lists & Looping', isSelect: selectIndex == 5, onPressed: () {
+            setState(() {
+              selectIndex = 5;
+            });
+          }),
+
+          _chipsRowItem(text: 'Slots', isSelect: selectIndex == 6, onPressed: () {
+            setState(() {
+              selectIndex = 6;
+            });
+          }),
+
+          _chipsRowItem(text: 'Styles', isSelect: selectIndex == 7, onPressed: () {
+            setState(() {
+              selectIndex = 7;
+            });
+          }),
+
+          _chipsRowItem(text: 'States', isSelect: selectIndex == 8, onPressed: () {
+            setState(() {
+              selectIndex = 8;
+            });
+          }),
+
+          _chipsRowItem(text: 'Scoped Data Propagation', isSelect: selectIndex == 9, onPressed: () {
+            setState(() {
+              selectIndex = 9;
+            });
+          }),
+
+          _chipsRowItem(text: 'Side Effects', isSelect: selectIndex == 10, onPressed: () {
+            setState(() {
+              selectIndex = 10;
+            });
+          }),
+        ],
+      ),
+    );
+  }
+
+  Widget _chipsRowItem({VoidCallback? onPressed, required String text, bool isSelect = false}) {
+    final style = isSelect ? ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.deepPurple)
+    ) : const ButtonStyle();
+    final textStyle = isSelect ? const TextStyle(color: Colors.white) : const TextStyle();
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: style,
+        child: Text(text, style: textStyle),
       ),
     );
   }
